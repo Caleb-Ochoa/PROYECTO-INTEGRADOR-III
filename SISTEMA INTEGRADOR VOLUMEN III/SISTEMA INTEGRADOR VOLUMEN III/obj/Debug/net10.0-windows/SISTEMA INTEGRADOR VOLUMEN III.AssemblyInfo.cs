459fbd418370e5532c2119da819dc43af54c7d37
Caleb-Ochoa <cochoar@unicesar.edu.co>
2026-05-21 16:03:03 -0500

    .

--- SISTEMA INTEGRADOR VOLUMEN III/SISTEMA INTEGRADOR VOLUMEN III/obj/Debug/net10.0-windows/SISTEMA INTEGRADOR VOLUMEN III.AssemblyInfo.cs
+++ SISTEMA INTEGRADOR VOLUMEN III/SISTEMA INTEGRADOR VOLUMEN III/obj/Debug/net10.0-windows/SISTEMA INTEGRADOR VOLUMEN III.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SISTEMA INTEGRADOR VOLUMEN III")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e4183564c766d4d71749c0f37ed7f15872fd02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf73ac84a014b49fa8475fc70c5b27f721de50b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SISTEMA INTEGRADOR VOLUMEN III")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SISTEMA INTEGRADOR VOLUMEN III")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
