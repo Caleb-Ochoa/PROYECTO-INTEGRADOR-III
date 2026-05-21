d3e4183564c766d4d71749c0f37ed7f15872fd02
DeivyMateus <deivyandres30@gmail.com>
2026-05-21 12:48:15 -0500

    B

--- SISTEMA INTEGRADOR VOLUMEN III/SISTEMA INTEGRADOR VOLUMEN III/Form1.Designer.cs
+++ SISTEMA INTEGRADOR VOLUMEN III/SISTEMA INTEGRADOR VOLUMEN III/Form1.Designer.cs
@@ -29,23 +29,36 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(346, 132);
+            label1.Font = new Font("Segoe UI", 28.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(395, 176);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(381, 62);
             label1.TabIndex = 0;
-            label1.Text = "label1";
+            label1.Text = "QUE MIRA BOBO";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(296, 75);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
+            Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -55,5 +68,6 @@
         #endregion
 
         private Label label1;
+        private Label label2;
     }
 }
