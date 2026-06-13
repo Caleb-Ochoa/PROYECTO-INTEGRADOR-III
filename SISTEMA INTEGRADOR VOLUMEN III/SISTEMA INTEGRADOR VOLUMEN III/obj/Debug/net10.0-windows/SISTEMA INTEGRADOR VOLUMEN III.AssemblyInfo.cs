ce905e3c2dbe2c491e7cc23d6836e85617d8f1f4
Gutierrezarenas28 estrellas <gutierrezarenas28@gmail.com>
2026-06-13 16:01:11 -0500

    .

--- SISTEMA INTEGRADOR VOLUMEN III/SISTEMA INTEGRADOR VOLUMEN III/obj/Debug/net10.0-windows/SISTEMA INTEGRADOR VOLUMEN III.AssemblyInfo.cs
+++ SISTEMA INTEGRADOR VOLUMEN III/SISTEMA INTEGRADOR VOLUMEN III/obj/Debug/net10.0-windows/SISTEMA INTEGRADOR VOLUMEN III.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SISTEMA INTEGRADOR VOLUMEN III")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d3531262c7c85297654228fce44b16ad2ab8d38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b51c915c1b380973d02c1f6c6f9beb57172fa10a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SISTEMA INTEGRADOR VOLUMEN III")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SISTEMA INTEGRADOR VOLUMEN III")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
