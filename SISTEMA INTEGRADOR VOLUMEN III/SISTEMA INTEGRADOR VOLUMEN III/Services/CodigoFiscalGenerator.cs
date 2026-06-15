c93177753bd7a37760777fa45d7b2f6958df961b
Caleb-Ochoa <cochoar@unicesar.edu.co>
2026-06-15 02:24:18 -0500

    .

--- SISTEMA INTEGRADOR VOLUMEN III/SISTEMA INTEGRADOR VOLUMEN III/Services/CodigoFiscalGenerator.cs
+++ SISTEMA INTEGRADOR VOLUMEN III/SISTEMA INTEGRADOR VOLUMEN III/Services/CodigoFiscalGenerator.cs
@@ -12,10 +12,8 @@ namespace SISTEMA_INTEGRADOR_VOLUMEN_III.Services
 
         public CodigoFiscalGenerator(DataManager<Factura> dm) => _dm = dm;
 
-        /// <summary>
         /// Genera un código único basado en la fecha y el siguiente Id de factura,
         /// garantizando que no haya duplicados aunque el programa se reinicie.
-        /// </summary>
         public string Generar()
         {
             int nextId = _dm.GetNextId();
